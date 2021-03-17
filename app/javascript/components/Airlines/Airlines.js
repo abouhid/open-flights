830d82122942ae2873f1a92f4bbc36daa98c9c88
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-03-17 12:03:40 -0300

    finish styling and video's part 2

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Airline from "./Airline";
+import styled from "styled-components";
+import Header from "./Header";
 
+const Home = styled.div`
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1200px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+  /* > div {
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+  } */
+`;
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
   useEffect(() => {
@@ -18,15 +39,11 @@ const Airlines = () => {
   });
 
   return (
-    <div className="home">
-      <div className="header">
-        <h1>OpenFlights</h1>
-        <div className="subheader">Honest, unbiased airline reviews</div>
-      </div>
-      <div className="grid">
-        <ul>{grid}</ul>
-      </div>
-    </div>
+    <Home>
+      <Header />
+
+      <Grid>{grid}</Grid>
+    </Home>
   );
 };
 
